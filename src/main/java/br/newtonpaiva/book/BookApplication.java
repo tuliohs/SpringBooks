e86e15ed8736191eb48515dbe7c8f8df21b22089
Tulio Henrique <tuliohhenriquepaz@gmail.com>
2020-09-25 02:06:15 -0300

    init updates

--- src/main/java/br/newtonpaiva/book/BookApplication.java
+++ src/main/java/br/newtonpaiva/book/BookApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookApplication {
+public class BookApplication  implements CommandLineRunner  {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookApplication.class, args);
+	public static void main(String[] args) {		
+		//SpringApplication.run(BookApplication.class, args);
 	}
 
 	@Override
