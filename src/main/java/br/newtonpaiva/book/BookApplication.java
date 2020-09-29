3e543c5b16968a1760201c814a292db05dd3212b
Tulio Henrique <tuliohhenriquepaz@gmail.com>
2020-09-29 20:04:57 -0300

    insert docker file

--- src/main/java/br/newtonpaiva/book/BookApplication.java
+++ src/main/java/br/newtonpaiva/book/BookApplication.java
@@ -30,3 +30,10 @@ public class BookApplication implements CommandLineRunner {
 		}
 	}
 }
+/*
+VOLUME /tmp
+EXPOSE 8080
+ARG JAR_FILE=build/libs/book-0.0.1-SNAPSHOT.jar
+ADD ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+*/
